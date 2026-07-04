11863a202bd99dfd88fb8a3ebf7b2ea1991bdafa
GithmiA <githmianuradha67@gmail.com>
2026-07-04 09:11:22 +0530

    Add initial project files and configurations
    - Created .vscode/settings.json to configure Live Server port.
    - Added nature.jpg image file.
    - Implemented app.js with a customer object and initial console log.
    - Created app.ts file for future TypeScript implementation.

---
+++ js/app.js
@@ -0,0 +1,48 @@
+
+// let name = "Saman";
+// let age = 18;
+// let isAfterAL = false;
+// let customers = {};
+// let customer = {};
+// let salary = 7500.0;
+
+// let x = 10;
+// let y = "10";
+// console.log(x===y);
+
+// let day = 1;
+
+//  switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//      case 2:
+//          console.log("Tuesday");
+//          break;
+//     case 3:
+//          console.log("Wednesday");        
+//         break;
+//      default:
+//         console.log("Invalid day");
+// }
+// function add(a,b){
+//    return a+b;
+// }
+// let result = add(10,20);
+
+// console.log(result);
+
+// let number = [1, 2, "three", 4, "five"];
+
+// for(let i=0;i<number.length;i++){
+//          console.log(number[i]);
+// }
+
+let customer = {
+    name: "Saman",
+    age: 18,
+    isAfterAL: false,
+    salary: 7500.0
+}
+
+console.log(customer);
