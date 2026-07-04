caf4fec42a3cb496fba025fcb259365841c9383c
GithmiA <githmianuradha67@gmail.com>
2026-07-04 11:58:27 +0530

    Add heading and button functionality to change text dynamically

--- js/app.js
+++ js/app.js
@@ -38,11 +38,71 @@
 //          console.log(number[i]);
 // }
 
-let customer = {
-    name: "Saman",
-    age: 18,
-    isAfterAL: false,
-    salary: 7500.0
-}
+//console.log("Invalid day");
 
-console.log(customer);
+
+// 
+let customer2 = {
+    name: "John",
+    age: 25,
+    address: "New York",
+    salary: 50000,
+    items: [
+        {
+            id: 1,
+            name: "item1",
+            price: 100,
+            variants: [
+                {
+                    id: 1,
+                    name: "variant1",
+                    price: 10
+                },
+                {
+                    id: 2,
+                    name: "variant2",
+                    price: 20
+                    
+                }
+            ]
+        }, {
+            id: 2,
+            name: "item2",
+            price: 200
+        }, {
+            id: 3,
+            name: "item3",
+            price: 300
+        }
+    ],
+    father: {
+        name: "Doe",
+        age: 50,
+        address: "New York"
+    }
+};
+// console.log(customer2.name);
+// console.log(customer2.age);
+// console.log(customer2.address);
+// console.log(customer2.salary);
+// console.log(customer2.items[2].name);
+// console.log(customer2.father.name);
+//console.log(customer2.items[0].variants[1].price);
+
+//console.log(document.title);
+//document.write("");
+ let heading = document.getElementById("heading");
+console.log(heading);
+ 
+ let number = 0;
+ function changeheading1(){
+   heading.innerText = "Githmi"+number++;
+   console.log("Clicked");
+ }
+ function changeheading2(){
+   heading.innerText = "Githmi"+number--;
+   console.log("Clicked");
+ }
+
+
+
